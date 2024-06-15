ab4c796679e2f82d7de5287d60553730776290a1
faysaltaysir <faysaltaysir1435@gmail.com>
2024-06-16 03:22:49 +0600

    update and select songs

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
 console.log("music app");
 
-let audioElement = new Audio('./assets/song1.mp3');
+let songs = [
+    {id: 0,name: "ashique 2",src: "songs/1.mp3", img: "./assets/card2img.jpeg", desc: "Your daily updates of most played ..."},
+    {id: 1,name: "Malang",src: "songs/2.mp3", img: "./assets/card3img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 2,name: "rangde basanti",src: "songs/3.mp3", img: "./assets/card4img.jpeg",desc: "Your daily updates of most played ..."},
+    // {name: "ashique ban aya",src: "songs/4.mp3", img: "songs/4.jpg",desc: "Your daily updates of most played ..."},
+    // {name: "ashique ban aya",src: "songs/4.mp3", img: "songs/4.jpg",desc: "Your daily updates of most played ..."}
+];
+console.log(songs[1]);
+for(song of songs){
+    console.log(song);   
+    let id = song.id; 
+
+    let playlist = document.querySelector(".card-container");
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("id",id);
+
+    let img = document.createElement("img");
+    img.classList.add("card-img");
+    img.src = song.img;
+    img.setAttribute("id",id);
+    card.appendChild(img);
+
+    let title = document.createElement("p");
+    title.classList.add("heading");
+    title.innerText = song.name;
+    title.setAttribute("id",id);
+    card.appendChild(title);
+
+    let desc = document.createElement("p");
+    desc.classList.add("card-about");
+    desc.innerText = song.desc;
+    card.appendChild(desc);
+    desc.setAttribute("id",id);
+
+    console.log(card.id);
+    playlist.appendChild(card);
+}
+let sgn = "";
+let card = document.querySelectorAll(".card");
+for(cd of card){
+    cd.addEventListener("click",(e)=>{
+        console.log(e.target.id);
+        sgn = songs[e.target.id].src;
+        console.log(sgn);
+        let playImg = document.querySelector(".music");
+        let title = document.querySelector(".title");
+        title.innerText = songs[e.target.id].name;
+        playImg.src = songs[e.target.id].img;
+        console.log(songs[e.target.id].img);
+    });
+}
+
+
+
+
+let audioElement = new Audio('./songs/1.mp3');
 let myProgress = document.querySelector(".play-progress");
 
 let musicgif = document.querySelector(".none");
 let count = 0;
 let playbtn = document.querySelector(".extra");
+
 playbtn.addEventListener("click",()=>{
     count++;
     if(audioElement.paused ||audioElement.currentTime<=0){
