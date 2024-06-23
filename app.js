18c3438c0cb0d3dc83c601da76f6b5343fac7270
faysaltaysir <faysaltaysir1435@gmail.com>
2024-06-23 12:12:45 +0600

    select play update change

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 console.log("music app");
 
 let songs = [
-    {id: 0,name: "ashique 2",src: "songs/1.mp3", img: "./assets/card2img.jpeg", desc: "Your daily updates of most played ..."},
-    {id: 1,name: "Malang",src: "songs/2.mp3", img: "./assets/card3img.jpeg",desc: "Your daily updates of most played ..."},
-    {id: 2,name: "rangde basanti",src: "songs/3.mp3", img: "./assets/card4img.jpeg",desc: "Your daily updates of most played ..."},
-    // {name: "ashique ban aya",src: "songs/4.mp3", img: "songs/4.jpg",desc: "Your daily updates of most played ..."},
-    // {name: "ashique ban aya",src: "songs/4.mp3", img: "songs/4.jpg",desc: "Your daily updates of most played ..."}
+    {id: 0,name: "ashique 2",src: "songs/1.mp3", img: "./assets/card1img.jpeg", desc: "Your daily updates of most played ..."},
+    {id: 1,name: "Malang",src: "songs/2.mp3", img: "./assets/card2img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 2,name: "rangde basanti",src: "songs/1.mp3", img: "./assets/card3img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 3,name: "ashique 2",src: "songs/2.mp3", img: "./assets/card4img.jpeg", desc: "Your daily updates of most played ..."},
+    {id: 4,name: "Malang",src: "songs/1.mp3", img: "./assets/card5img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 5,name: "rangde basanti",src: "songs/2.mp3", img: "./assets/card6img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 6,name: "ashique 2",src: "songs/1.mp3", img: "./assets/card2img.jpeg", desc: "Your daily updates of most played ..."},
+    {id: 7,name: "Malang",src: "songs/2.mp3", img: "./assets/card3img.jpeg",desc: "Your daily updates of most played ..."},
+    {id: 8,name: "rangde basanti",src: "songs/1.mp3", img: "./assets/card4img.jpeg",desc: "Your daily updates of most played ..."},
+    
 ];
 console.log(songs[1]);
 for(song of songs){
@@ -41,16 +46,44 @@ for(song of songs){
 }
 let sgn = "";
 let card = document.querySelectorAll(".card");
-let audioElement = null;
+let audioElement;
 let playbtn = document.querySelector(".extra");
 
 let musicgif = document.querySelector(".none");
+let memo = null;
+
+let myProgress = document.querySelector(".play-progress");
+
+let count = 0;
+let finishTime = document.querySelector(".finishTime");
+let memoduration;
 for(cd of card){
+    
     cd.addEventListener("click",async (e)=>{
-        // let sel = document.querySelectorAll(".select");
-        // for(e of sel){
-        //     e.classList.remove("select");
-        // }
+        if(memo){  //select deselect
+            memo.classList.remove("select");
+            console.log(memo);
+        }
+        console.log(memo==e.currentTarget);
+        
+        if(audioElement){
+            // if(memo != e.currentTarget){
+                audioElement.pause();
+                playbtn.classList.add("fa-circle-play");
+                playbtn.classList.remove("fa-circle-pause");
+                musicgif.style.opacity = 0;
+                
+            // }
+            // audioElement.pause();
+            // e.currentTarget.classList.remove("select");
+            // playbtn.classList.add("fa-circle-play");
+            // playbtn.classList.remove("fa-circle-pause");
+            // musicgif.style.opacity = 0;
+            // playbtn.style.opacity = 0;
+
+        }
+        console.log(cd);
+       
         console.log(e.currentTarget); //current target takes on which the event listener
         sgn = `./${songs[e.target.id].src}`;
         console.log(sgn);
@@ -59,19 +92,15 @@ for(cd of card){
         title.innerText = songs[e.target.id].name;
         playImg.src = songs[e.target.id].img;
         console.log(songs[e.target.id].img);
-        if(audioElement){
-            audioElement.pause();
-            // e.currentTarget.classList.remove("select");
-            playbtn.classList.add("fa-circle-play");
-            playbtn.classList.remove("fa-circle-pause");
-            musicgif.style.opacity = 0;
-            // playbtn.style.opacity = 0;
-
-        }
-
-         
+        e.currentTarget.classList.add("select");
+        memo = e.currentTarget;
+        console.log("audio",audioElement);
+        console.log("memo",memo);
+        
+        
         audioElement = new Audio(sgn);
         // e.currentTarget.classList.add("select");   
+        progressUpdate();
         
     });
 }
@@ -79,14 +108,13 @@ for(cd of card){
 
 
 
-let myProgress = document.querySelector(".play-progress");
-
-let count = 0;
 
 playbtn.addEventListener("click",async ()=>{
     count++;
     if(audioElement.paused ||audioElement.currentTime<=0){
         await audioElement.play();
+        console.log(audioElement.duration);
+        memoduration = audioElement.duration.toFixed(0);
         playbtn.classList.remove("fa-circle-play");
         playbtn.classList.add("fa-circle-pause");
         musicgif.style.opacity = 1;
@@ -97,11 +125,12 @@ playbtn.addEventListener("click",async ()=>{
         playbtn.classList.add("fa-circle-play");
         musicgif.style.opacity = 0;
     }
+    progressUpdate();
 });
 
 let currentTime = document.querySelector(".currentTime");
-let finishTime = document.querySelector(".finishTime");
-myProgress.addEventListener('change',()=>{
+
+myProgress.addEventListener('change',async()=> {
     audioElement.currentTime = (myProgress.value/100)*audioElement.duration;
     console.log(audioElement.currentTime);
 });
@@ -117,13 +146,20 @@ function timeConv(time){
     }
     return timeString;
 }
-audioElement.addEventListener("timeupdate",()=>{
-    let time1 = (audioElement.currentTime).toFixed(0);
-    currentTime.innerText = timeConv(time1); 
-    let time2 = (audioElement.duration).toFixed(0);
-    finishTime.innerText = timeConv(time2);
-    let progress = parseInt((time1/time2)*100);
-    myProgress.value = progress;
-});
+let progress;
+function progressUpdate(){
+    audioElement.addEventListener("timeupdate",async()=>{
 
+        let time1 = (audioElement.currentTime).toFixed(0);
+        currentTime.innerText = timeConv(time1); 
+        let time2 = (audioElement.duration).toFixed(0);
+        if(time2 == "NaN")
+            time2 = memoduration;     
+        finishTime.innerText = timeConv(time2);
+        progress = parseInt((time1/time2)*100);
+        myProgress.value = progress;
+        console.log(time1,time2);
+    });
+        
+}
 
